d9c6b960263cf8347b1d7c6ee643601948a715bb
jatinAgarwala <jatin.a@research.iiit.ac.in>
2022-12-29 02:26:33 +0530

    Fix typos

--- Classification/ADA/svm.py
+++ Classification/ADA/svm.py
@@ -2,23 +2,26 @@ import numpy as np
 from sklearn import svm
 
 from data import train, test, valid
+from data import PreProcessing
 
-# Convert the training and test sets to NumPy arrays
-X_train = np.array(train[0])
-y_train = np.array(train[1])
-X_test = np.array(valid[0])
-y_test = np.array(valid[1])
+for guilty in [True, False]:
+    train, valid, test = PreProcessing('cases_2010.csv', guilty)
+    # Convert the training and test sets to NumPy arrays
+    X_train = np.array(train[0])
+    y_train = np.array(train[1])
+    X_test = np.array(valid[0])
+    y_test = np.array(valid[1])
 
-# Create an SVM classifier
-clf = svm.SVC(kernel='linear')
+    # Create an SVM classifier
+    clf = svm.SVC(kernel='linear')
 
-# Train the classifier on the training set
-clf.fit(X_train, y_train)
+    # Train the classifier on the training set
+    clf.fit(X_train, y_train)
 
-# Test the classifier on the test set
-accuracy = clf.score(X_test, y_test)
+    # Test the classifier on the test set
+    accuracy = clf.score(X_test, y_test)
 
-print('Test accuracy: {:.2f}%'.format(accuracy * 100))
+    print('Test accuracy: {:.2f}%'.format(accuracy * 100))
 
-# Make predictions on new data
-predictions = clf.predict(test[0])
+    # Make predictions on new data
+    predictions = clf.predict(test[0])
