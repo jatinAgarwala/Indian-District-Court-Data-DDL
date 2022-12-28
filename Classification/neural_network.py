d050f38a71dae0b713d2e427b63239ebde079892
jatinAgarwala <jatin.a@research.iiit.ac.in>
2022-12-29 01:25:02 +0530

    Add code for Classification

---
+++ Classification/neural_network.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+
+from tqdm import tqdm
+
+from torch.utils.data import DataLoader
+from data import PreProcessing, DataDispFem, DataDispNoFem
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+dataset_classes = [DataDispFem, DataDispNoFem]
+
+csv_path = '../../csv/'
+YEAR = '2010'
+cases_path = csv_path + 'cases/cases/cases_' + YEAR + '.csv'
+
+for guilty in [False, True]:
+    train, valid, test = PreProcessing(cases_path, guilty)
+    dc_i = 0
+    for dc in dataset_classes:
+        train_dataloader = DataLoader(dc(train), batch_size=128).to(DEVICE)
+        valid_dataloader = DataLoader(dc(valid), batch_size=128).to(DEVICE)
+
+        class NeuralNet(nn.Module):
+            def __init__(self, input_size, hidden_size, num_classes):
+                super(NeuralNet, self).__init__()
+                self.fc1 = nn.Linear(input_size, hidden_size)
+                self.relu = nn.ReLU()
+                self.fc2 = nn.Linear(hidden_size, num_classes)
+            
+            def forward(self, x):
+                out = self.fc1(x)
+                out = self.relu(out)
+                out = self.fc2(out)
+                return out
+
+        if dc == DataDispFem:
+            model = NeuralNet(13, 128, 52).to(DEVICE)
+        else:
+            model = NeuralNet(9, 128, 52).to(DEVICE)
+            
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+        num_epochs = 2
+        for epoch in range(num_epochs):
+            for batch in tqdm(train_dataloader, desc="Training"):
+                outputs = model(batch[0])
+                loss = criterion(outputs, batch[1])
+                # print(f"Training Loss: {loss}")
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for batch in tqdm(valid_dataloader, desc="Validating"):
+                    outputs = model(batch[0])
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += batch[1].size(0)
+                    correct += (predicted == batch[1]).sum().item()
+            
+            print('Epoch [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+                .format(epoch+1, num_epochs, loss.item(), 100*correct/total))
+
+        test_dataloader = DataLoader(DataDispFem(test), batch_size=128).to(DEVICE)
+
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch in tqdm(test_dataloader, desc="Testing"):
+                outputs = model(batch[0])
+                _, predicted = torch.max(outputs.data, 1)
+                total += batch[1].size(0)
+                correct += (predicted == batch[1]).sum().item()
+
+        print('Test Accuracy: {:.2f}%'.format(100*correct/total))
+
+        if dc_i == 0:
+            if guilty:
+                torch.save(model.state_dict(), "guilty_model_fem.pth")
+            else:
+                torch.save(model.state_dict(), "model_fem.pth")
+        else:
+            if guilty:
+                torch.save(model.state_dict(), "guilty_model_no_fem.pth")
+            else:
+                torch.save(model.state_dict(), "model_no_fem.pth")
